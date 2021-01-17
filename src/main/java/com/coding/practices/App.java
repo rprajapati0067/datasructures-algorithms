9930819982439da8d4510ff3e43ba27ef4f48228
ravi <rprajapati067@gmail.com>
2021-01-17 10:45:22 +0530

    doumentation correction and sum of two number code added

--- src/main/java/com/coding/practices/App.java
+++ src/main/java/com/coding/practices/App.java
@@ -7,7 +7,7 @@ import com.coding.practices.warmups.Warmups;
  *
  */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// Minimum number in array
 		System.out.println(Warmups.minNumber(new int[] { 2, 1, 3, 6, 4, 8 }));
 
@@ -16,6 +16,9 @@ public class App {
 
 		// Minimum and Maximum number in array
 		System.out.println(Warmups.minMaxNumber(new int[] { 2, 1, 3, 6, 4, 8 }));
+
+		// Sum of two number equal to k
+		System.out.println(Warmups.sumOfTwoNum(new int[] { 2, 1, 3, 6, 4, 8 }, 9));
 
 	}
 
--- src/main/java/com/coding/practices/warmups/Warmups.java
+++ src/main/java/com/coding/practices/warmups/Warmups.java
@@ -1,8 +1,13 @@
 package com.coding.practices.warmups;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Warmups {
 
-	// [2, 1, 3, 6, 4, 8]
+	/*
+	 * Minimum number in array, input: [2, 1, 3, 6, 4, 8]
+	 */
 	public static int minNumber(int[] arr) {
 		int min = 0;
 		if (arr != null) {
@@ -16,7 +21,9 @@ public class Warmups {
 		return min;
 	}
 
-	// [2, 1, 3, 6, 4, 8]
+	/*
+	 * Maximum number in array, input: [2, 1, 3, 6, 4, 8]
+	 */
 	public static int maxNumber(int[] arr) {
 		int max = 0;
 		if (arr != null) {
@@ -30,7 +37,10 @@ public class Warmups {
 		return max;
 	}
 
-	// [2, 1, 3, 6, 4, 8]
+	/*
+	 * Minimum and Maximum number in array(Single Iteration), input: [2, 1, 3, 6, 4,
+	 * 8]
+	 */
 	public static String minMaxNumber(int[] arr) {
 		int min = 0;
 		int max = 0;
@@ -46,5 +56,24 @@ public class Warmups {
 			}
 		}
 		return "[Min: " + min + ", " + "Max: " + max + "]";
+	}
+
+	/*
+	 * Sum of two number equal to k, input: [2, 1, 3, 6, 4, 8]
+	 */
+	public static Map<Integer, Integer> sumOfTwoNum(int[] arr, int k) {
+		Map<Integer, Integer> pairs = new HashMap<Integer, Integer>();
+		if (arr != null) {
+
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = i + 1; j < arr.length; j++) {
+					if (arr[i] + arr[j] == k) {
+						pairs.put(arr[i], arr[j]);
+					}
+				}
+
+			}
+		}
+		return pairs;
 	}
 }
