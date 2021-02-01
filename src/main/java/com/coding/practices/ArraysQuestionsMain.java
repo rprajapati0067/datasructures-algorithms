1578ee76a7802b917f581b0e03f07463b8d6bf2a
ravi <rprajapati067@gmail.com>
2021-02-01 19:15:16 +0530

    More arrays problems added

--- src/main/java/com/coding/practices/ArraysQuestionsMain.java
+++ src/main/java/com/coding/practices/ArraysQuestionsMain.java
@@ -45,6 +45,61 @@ public class ArraysQuestionsMain {
 
 			System.out.print(replacedLeftCurrentElement[i] + " ");
 		}
+
+		// Arrange the elements in zigzag manner like < > < > or > < > <
+		int[] result1 = ArraysQuestions.zigzag1(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+		System.out.println();
+		for (int i = 0; i < result1.length; i++) {
+
+			System.out.print(result1[i] + " ");
+		}
+		// Arrange the elements in zigzag manner like < > < > or > < > <
+		int[] result2 = ArraysQuestions.zigzag2(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+		System.out.println();
+		for (int i = 0; i < result2.length; i++) {
+
+			System.out.print(result2[i] + " ");
+		}
+		// Arrange the elements in zigzag manner like < > < > or > < > <
+		int[] result3 = ArraysQuestions.zigzag3(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+		System.out.println();
+		for (int i = 0; i < result3.length; i++) {
+
+			System.out.print(result3[i] + " ");
+		}
+
+		// Arrange one first and the zero
+		int[] result4 = ArraysQuestions.arrageOneZero1(new int[] { 1, 0, 1, 1, 0, 0, 1, 0, 1 });
+		System.out.println();
+		for (int i = 0; i < result4.length; i++) {
+
+			System.out.print(result4[i] + " ");
+		}
+		// Arrange one first and the zero
+		int[] result5 = ArraysQuestions.arrageOneZero2(new int[] { 1, 0, 1, 1, 0, 0, 1, 0, 1 });
+		System.out.println();
+		for (int i = 0; i < result5.length; i++) {
+
+			System.out.print(result5[i] + " ");
+		}
+		// Arrange one first and the zero
+		int[] result6 = ArraysQuestions.arrageOneZero3(new int[] { 1, 0, 1, 1, 0, 0, 1, 0, 1 });
+		System.out.println();
+		for (int i = 0; i < result6.length; i++) {
+
+			System.out.print(result6[i] + " ");
+		}
+
+		System.out.println();
+		// Arrange one first and the zero
+		System.out.println(ArraysQuestions.findMissingNumber1(new int[] { 3, 1, 2, 5 }));
+		// Arrange one first and the zero
+		System.out.println(ArraysQuestions.findMissingNumber2(new int[] { 3, 1, 2, 5 }));
+		// Arrange one first and the zero
+		System.out.println(ArraysQuestions.findMissingNumber3(new int[] { 3, 1, 2, 5 }));
+		// Arrange one first and the zero
+		System.out.println(ArraysQuestions.findMissingNumber4(new int[] { 3, 1, 2, 5 }));
+
 	}
 
 }
--- src/main/java/com/coding/practices/arrays/ArraysQuestions.java
+++ src/main/java/com/coding/practices/arrays/ArraysQuestions.java
@@ -1,5 +1,10 @@
 package com.coding.practices.arrays;
 
+import java.security.KeyStore.Entry;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArraysQuestions {
 
 	/*
@@ -132,6 +137,202 @@ public class ArraysQuestions {
 			}
 		}
 		return arr1;
+	}
+
+	/*
+	 * 
+	 * Arrange the elements in zigzag manner like < > < > or > < > < input: [ 4, 3,
+	 * 2, 1, 6, 7 ], output: [1 < 3 > 2 < 6 > 4 < 7]
+	 * 
+	 */
+	public static int[] zigzag1(int[] arr) {
+		Arrays.sort(arr);
+		for (int i = 1; i < arr.length - 1; i++) {
+			int temp = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i + 1] = temp;
+
+			i++;
+		}
+
+		return arr;
+	}
+
+	/*
+	 * 
+	 * Arrange the elements in zigzag manner like > < > < input: [ 4, 3, 2, 1, 6, 7
+	 * ], output: [2 > 1 < 4 > 3 < 7 >6]
+	 * 
+	 */
+	public static int[] zigzag2(int[] arr) {
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length - 1; i++) {
+			int temp = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i + 1] = temp;
+			i++;
+		}
+
+		return arr;
+	}
+
+	/*
+	 * 
+	 * Arrange the elements in zigzag manner like > < > < input: [ 4, 3, 2, 1, 6, 7
+	 * ], output: [2 > 1 < 4 > 3 < 7 >6]
+	 * 
+	 */
+	public static int[] zigzag3(int[] arr) {
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (i % 2 == 0) {
+				if (arr[i] > arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				} else {
+					if (arr[i] < arr[i + 1]) {
+
+						int temp = arr[i];
+						arr[i] = arr[i + 1];
+						arr[i + 1] = temp;
+					}
+				}
+
+			}
+		}
+
+		return arr;
+	}
+
+	/*
+	 * 
+	 * Arrange the arrage One first and then Zero input: [1, 0, 1, 1, 0, 0, 1, 0, 1]
+	 * output:[1, 1, 1, 1, 1, 0, 0, 0, 0]
+	 * 
+	 */
+	public static int[] arrageOneZero1(int[] arr) {
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 1) {
+				count++;
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if (count > 0) {
+				arr[i] = 1;
+				count--;
+			} else {
+				arr[i] = 0;
+			}
+		}
+
+		return arr;
+	}
+
+	public static int[] arrageOneZero2(int[] arr) {
+
+		int i = 0, j = 0;
+
+		while (j < arr.length) {
+			if (arr[j] == 1) {
+				arr[i] = 1;
+				i++;
+				j++;
+			} else {
+				j++;
+			}
+		}
+		while (i < arr.length) {
+			arr[i] = 0;
+			i++;
+		}
+
+		return arr;
+	}
+
+	public static int[] arrageOneZero3(int[] arr) {
+
+		int i = 0;
+		int j = arr.length - 1;
+
+		while (i < j) {
+			if (arr[i] == 0 && arr[j] == 0) {
+				j--;
+			} else if (arr[i] == 1 && arr[j] == 1) {
+				i++;
+			} else if (arr[i] == 0 && arr[j] == 1) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			} else {
+				i++;
+			}
+
+		}
+
+		return arr;
+	}
+
+	/*
+	 * 
+	 * Find missing number in the series input: [1, 0, 1, 1, 0, 0, 1, 0, 1]
+	 * output:[1, 1, 1, 1, 1, 0, 0, 0, 0]
+	 * 
+	 */
+	public static int findMissingNumber1(int[] arr) {
+		int n = arr.length + 1;
+		int idealSum = (n * (n + 1)) / 2;
+		int actualSum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			actualSum += arr[i];
+		}
+		return idealSum - actualSum;
+
+	}
+
+	public static int findMissingNumber2(int[] arr) {
+		int xor = 0;
+		for (int i : arr) {
+			xor = xor ^ i;
+		}
+		for (int i = 1; i <= arr.length + 1; i++) {
+			xor = xor ^ i;
+		}
+		return xor;
+	}
+
+	public static int findMissingNumber3(int[] arr) {
+		int x = 0;
+		int y = 0;
+		for (int i : arr) {
+			x = x ^ i;
+		}
+		for (int i = 1; i <= arr.length + 1; i++) {
+			y = y ^ i;
+		}
+		return x ^ y;
+	}
+
+	public static int findMissingNumber4(int[] arr) {
+		int result = -1;
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			map.put(arr[i], i);
+		}
+
+		for (int i = 1; i < arr.length + 1; i++) {
+			if (!map.containsKey(i)) {
+				result = i;
+				return result;
+			}
+		}
+		return result;
 	}
 
 }
